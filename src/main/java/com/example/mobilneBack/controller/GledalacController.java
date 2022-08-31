77a33ef26a338a365e0dcf9ec8c729d8d63b2c60
boris <djukanovicboris99@gmail.com>
2022-08-31 13:49:14 +0200

    Adding into db

--- src/main/java/com/example/mobilneBack/controller/GledalacController.java
+++ src/main/java/com/example/mobilneBack/controller/GledalacController.java
@@ -1,9 +1,19 @@
 package com.example.mobilneBack.controller;
 
+import com.example.mobilneBack.entity.Gledalac;
 import com.example.mobilneBack.service.GledalacService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GledalacController {
+    @Autowired
     private GledalacService gledalacService;
+
+    @PostMapping("/addGledalac")
+    public Gledalac addGledalac(@RequestBody Gledalac gledalac){
+        return gledalacService.addGledalac(gledalac);
+    }
 }
--- src/main/java/com/example/mobilneBack/service/GledalacService.java
+++ src/main/java/com/example/mobilneBack/service/GledalacService.java
@@ -1,7 +1,18 @@
 package com.example.mobilneBack.service;
 
+import com.example.mobilneBack.entity.Gledalac;
+import com.example.mobilneBack.repository.GledalacRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GledalacService {
+
+    @Autowired
+    private GledalacRepository gledalacRepository;
+
+    public Gledalac addGledalac(Gledalac gledalac){
+        return gledalacRepository.save(gledalac);
+    }
+
 }
